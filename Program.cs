271cf298a042257fc53d7e3749bd814b888e6054
Tuba <tubaazsoy@gmail.com>
2024-11-21 01:10:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int kenar1, kenar2;
+
+Console.WriteLine("1.kenarı giriniz");
+kenar1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2.kenarı giriniz");
+kenar2 = Convert.ToInt32(Console.ReadLine());
+
+
+int alan = kenar1 * kenar2;
+
+Console.WriteLine("Karenin alanı: " + alan);
+
+Console.ReadKey();
